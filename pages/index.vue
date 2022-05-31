22f601c026557180c45a0d25830e9a523221bc5a
soon <919153908@qq.com>
2022-05-31 23:12:46 +0800

    Configuration to deploy to heroku

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,47 @@
 <template>
   <div>
-    <h1>Events</h1>
+    <h4>Events</h4>
+    <event-card
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+import { mapState } from 'vuex'
+
+export default {
+  head() {
+    return {
+      title: 'Event Listing'
+    }
+  },
+  async fetch({ store, error }) {
+    try {
+      await store.dispatch('events/fetchEvents')
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch events. Please try again.'
+      })
+    }
+  },
+  components: {
+    EventCard
+  },
+  /* 
+  computed: mapState({
+    events: state => state.events.events
+  })
+  */
+  computed: {
+    ...mapState('events', {
+      events: 'events'
+    })
+  }
+}
+</script>
